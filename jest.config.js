e3c15ded5b0ebff8efe22ccb297d8350450ff65a
Harry Tran <nhattq.coding@gmail.com>
2021-08-28 23:20:40 +0700

    main test(unit): add coverage for some function

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,12 @@ module.exports = {
     __DEV__: false,
     __TEST__: true,
   },
+  rootDir: './',
   testEnvironment: 'node',
   transform: {
     '^.+\\.tsx?$': '@sucrase/jest-plugin',
   },
   testPathIgnorePatterns: ['/node_modules/', '/dist/', '/types/'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleFileExtensions: ['js', 'ts', 'tsx', 'json', 'node'],
+  testMatch: ['<rootDir>/test/**/*.spec.ts'],
 };
---
+++ test/math.spec.ts
@@ -0,0 +1,10 @@
+import { calculateSum } from '../src/math';
+
+it('Calculate sum', () => {
+  expect(calculateSum(1, 2, 3)).toEqual(6);
+  expect(calculateSum([1, 2, 3])).toEqual(6);
+  expect(calculateSum([1], [2, 3])).toEqual(6);
+
+  // @ts-expect-error Bcs the type of args can be number [][]
+  expect(calculateSum(1, 2, [1, 2, 3])).toEqual(9);
+});

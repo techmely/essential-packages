670959dc8b64017aec98cda47d9bf8c81452d8e3
Harry Tran <nhattq.coding@gmail.com>
2021-08-28 23:46:01 +0700

    main workflow: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
   extends: ['@techmely/eslint-config-ts'],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    parser: '@typescript-eslint/parser',
+    project: ['tsconfig.json'],
+    ecmaFeatures: {
+      modules: true,
+    },
+  },
 };

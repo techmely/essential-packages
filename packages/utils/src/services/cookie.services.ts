070bc6bfc951bd5dfcd60243bdf3f44468ef0c1b
Harry Tran <nhattq.coding@gmail.com>
2021-09-23 07:19:57 +0700

    main feat(utils): add cookie service

---
+++ packages/utils/src/services/cookie.services.ts
@@ -0,0 +1,59 @@
+// eslint-disable-next-line max-classes-per-file
+
+import { Undefinable } from '@techmely/types';
+
+import { jwtDecodeToken } from '../_external/jwt';
+import { CookieInstance } from '../types';
+
+export class CookieService<T> implements CookieInstance<T> {
+  public appEnv: 'uat' | 'production' | 'staging';
+
+  public cookieDomain: string;
+
+  public cookieTokenName: string;
+
+  public constructor(env: 'uat' | 'production' | 'staging', cookieDomain: string) {
+    this.appEnv = env;
+    this.cookieDomain = cookieDomain;
+    this.cookieTokenName = `token_${this.appEnv}`;
+  }
+
+  setSecureToken(token: string) {
+    document.cookie =
+      this.appEnv === 'production'
+        ? `${this.cookieTokenName}=${token}; path=/; Domain=${this.cookieDomain}; Secure`
+        : `${this.cookieTokenName}=${token}; path=/; Secure`;
+  }
+
+  /**
+   * {@link https://stackoverflow.com/a/15724300}
+   *
+   * @param {string} name - the key name
+   * @returns {Undefinable<string>} the value in cookie
+   */
+  get(name: string) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop()?.split(';').shift();
+    return undefined;
+  }
+
+  getSecureToken() {
+    const token = this.get(this.cookieTokenName);
+    if (token) return token;
+    return '';
+  }
+
+  getDecodedSecureToken() {
+    const token = this.getSecureToken();
+    if (token) return jwtDecodeToken(token) as T;
+    return {} as T;
+  }
+
+  clearSecureToken() {
+    document.cookie =
+      this.appEnv === 'production'
+        ? `${this.cookieTokenName}=; path=/; Domain=${this.cookieDomain}; Secure`
+        : `${this.cookieTokenName}=; path=/; Secure`;
+  }
+}
--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -1,15 +1,27 @@
+import { Undefinable } from '@techmely/types';
+
 export type MergeInsertions<T> = T extends Record<string, any>
   ? { [K in keyof T]: MergeInsertions<T[K]> }
   : T;
 
-export type DeepMerge<F, S> = MergeInsertions<
-  {
-    [K in keyof F | keyof S]: K extends keyof S & keyof F
-      ? DeepMerge<F[K], S[K]>
-      : K extends keyof S
-      ? S[K]
-      : K extends keyof F
-      ? F[K]
-      : never;
-  }
->;
+export type DeepMerge<F, S> = MergeInsertions<{
+  [K in keyof F | keyof S]: K extends keyof S & keyof F
+    ? DeepMerge<F[K], S[K]>
+    : K extends keyof S
+    ? S[K]
+    : K extends keyof F
+    ? F[K]
+    : never;
+}>;
+
+export abstract class CookieInstance<T> {
+  abstract setSecureToken(token: string, env?: string): void;
+
+  abstract get(name: string): Undefinable<string>;
+
+  abstract getSecureToken(env: string): string;
+
+  abstract getDecodedSecureToken(): T;
+
+  abstract clearSecureToken(env: string): void;
+}

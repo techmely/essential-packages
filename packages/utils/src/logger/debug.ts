385d5117b5bb7949eb12e2c52eef09c74fe4b387
Harry <nhattq.coding@gmail.com>
2021-11-27 17:38:37 +0700

    feat(utils): add basic logger function

---
+++ packages/utils/src/logger/debug.ts
@@ -0,0 +1,21 @@
+import debug from 'debug';
+
+interface DebuggerOptions {
+  onlyWhenFocused?: boolean | string;
+}
+export type TechmelyDebugScope = `techmely:${string}`;
+
+export function createDebugger(
+  namespace: TechmelyDebugScope,
+  options: DebuggerOptions = {},
+): debug.Debugger['log'] {
+  const log = debug(namespace);
+  const { onlyWhenFocused } = options;
+  const focus = typeof onlyWhenFocused === 'string' ? onlyWhenFocused : namespace;
+  return (msg: string, ...args: any[]) => {
+    if (onlyWhenFocused) {
+      return;
+    }
+    log(msg, ...args);
+  };
+}

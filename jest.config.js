7fa4a997d9b5f654d2ce1c0ec12280f5eb4c8899
Harry <nhattq.coding@gmail.com>
2021-11-28 12:54:03 +0700

    workflow: move to pnp instead node-modules

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,17 @@
 const presetJest = require('./jest.preset');
 
-module.exports = { ...presetJest };
+module.exports = {
+  ...presetJest,
+  projects: [
+    {
+      displayName: 'Utils Package',
+      testEnvironment: 'jsdom',
+      testMatch: ['<rootDir>/packages/utils/test/**/*.test.ts'],
+    },
+    {
+      displayName: 'vite-plugin-runtime-env',
+      testEnvironment: 'node',
+      testMatch: ['<rootDir>/packages/vite-plugin/runtime-env/src/**/*.spec.ts'],
+    },
+  ],
+};

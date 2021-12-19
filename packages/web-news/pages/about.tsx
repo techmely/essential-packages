3829d89a912e7680d76423202f117a40a9cb6417
Harry <nhattq.coding@gmail.com>
2021-12-19 17:36:20 +0700

    main chore: update code

--- packages/web-news/pages/about.tsx
+++ packages/web-news/pages/about.tsx
@@ -1,33 +1,38 @@
-import Box from '@mui/material/Box';
+import type { NextPage } from 'next';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import { type NextPage } from 'next';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
-import Copyright from '../components/Copyright';
-import Link from '../components/Link';
-import ProTip from '../components/ProTip';
+import Link from '@web-news/components/Link';
+import ProTip from '@web-news/components/ProTip';
+import Copyright from '@web-news/components/Copyright';
 
-const Home: NextPage = () => (
-  <Container maxWidth="lg">
-    <Box
-      sx={{
-        my: 4,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Typography variant="h4" component="h1" gutterBottom>
-        MUI v5 + Next.js with TypeScript example
-      </Typography>
-      <Link href="/about" color="secondary">
-        Go to the about page
-      </Link>
-      <ProTip />
-      <Copyright />
-    </Box>
-  </Container>
-);
+const About: NextPage = () => {
+  return (
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h4" component="h1" gutterBottom>
+          MUI v5 + Next.js with TypeScript example
+        </Typography>
+        <Box maxWidth="sm">
+          <Button variant="contained" component={Link} noLinkStyle href="/">
+            Go to the home page
+          </Button>
+        </Box>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  );
+};
 
-export default Home;
+export default About;
--- packages/web-news/styles/theme.ts
+++ packages/web-news/styles/theme.ts
@@ -1,5 +1,19 @@
-import createCache from '@emotion/cache';
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
 
-export default function createEmotionCache() {
-  return createCache({ key: 'css' });
-}
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+});
+
+export default theme;

8c2c79c0d470735689d4480a377bd090e4e3d38f
Harry <nhattq.coding@gmail.com>
2022-01-10 05:27:55 +0700

    feat(utils): update invarient functions

--- packages/utils/src/logger/invariant.ts
+++ packages/utils/src/logger/invariant.ts
@@ -1,33 +1,33 @@
-/**
- * Use invariant() to assert state which your program assumes to be true.
- *
- * Provide sprintf-style format (only %s is supported) and arguments
- * to provide information about what broke and what you were
- * expecting.
- *
- * The invariant message will be stripped in production, but the invariant
- * will remain to ensure logic does not differ in production.
- *
- * @param {any} condition - The condition to verify.
- * @param {string} message - The format will throw err
- * @param {...any} args - arguments
- */
-export const invariant = (condition: any, message?: string, ...args: Array<any>) => {
-  if (condition) {
-    let error;
-    if (message === undefined) {
-      error = new Error(
-        'Minified exception occurred; use the non-minified dev environment ' +
-          'for the full error message and additional helpful warnings.',
-      );
-    } else {
-      let argIndex = 0;
-      // eslint-disable-next-line no-plusplus
-      error = new Error(message.replace(/%s/g, () => args[argIndex++]));
-      error.name = 'Invariant Violation';
-    }
+const isProduction: boolean = process.env.NODE_ENV === 'production';
+const prefix: string = 'Invariant failed';
 
-    error.framesToPop = 1;
-    throw error;
+// Throw an error if the condition fails
+// Strip out error messages for production
+// > Not providing an inline default argument for message as the result is smaller
+export default function invariant(
+  condition: any,
+  // Can provide a string, or a function that returns a string for cases where
+  // the message takes a fair amount of effort to compute
+  message?: string | (() => string),
+): asserts condition {
+  if (condition) {
+    return;
   }
-};
+  // Condition not passed
+
+  // In production we strip the message but still throw
+  if (isProduction) {
+    throw new Error(prefix);
+  }
+
+  // When not in production we allow the message to pass through
+  // *This block will be removed in production builds*
+
+  const provided: string | undefined = typeof message === 'function' ? message() : message;
+
+  // Options:
+  // 1. message provided: `${prefix}: ${provided}`
+  // 2. message not provided: prefix
+  const value: string = provided ? `${prefix}: ${provided}` : prefix;
+  throw new Error(value);
+}

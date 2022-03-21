35bc8e151975db2dfc12726484403634f2bb452c
Harry Tran <nhattq.coding@gmail.com>
2022-03-21 07:53:03 +0700

    feat(utils): add func convert percent to hext

--- packages/utils/src/transform/convert.ts
+++ packages/utils/src/transform/convert.ts
@@ -22,3 +22,18 @@ export function intToBuffer(integer: number | bigint): Buffer {
   const hex = intToHex(integer);
   return Buffer.from(hex, 'hex');
 }
+
+/**
+ * Transform an integer into a hex --> The opacity
+ *
+ * @param {number} percent the input value
+ * @returns {string} The hex decimal
+ */
+export function percentToHex(percent: number): string {
+  if (percent < 0 || percent > 100)
+    throw new Error('Value must in range [0, 100]');
+
+  const intValue = Math.round((percent / 100) * 255); // map percent to nearest integer (0 - 255)
+  const hexValue = intValue.toString(16); // get hexadecimal representation
+  return hexValue.padStart(2, '0').toUpperCase(); // format with leading 0 and upper case characters
+}

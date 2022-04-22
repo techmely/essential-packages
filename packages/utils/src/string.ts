0248fe372129f3953211ab5032b6bdbe0b3ba7aa
Harry <nhattq.coding@gmail.com>
2022-04-22 21:10:38 +0700

    feat(utils): add convertor percent to hext fuction

--- packages/utils/src/string.ts
+++ packages/utils/src/string.ts
@@ -5,3 +5,18 @@ export function snake2camel(src: string) {
 export function capitalizeFirst(value: string) {
   return value.replace(/^./, value[0].toUpperCase());
 }
+
+/**
+ * Transform an integer into a hex --> The opacity
+ *
+ * @param {number} percent the input value
+ * @returns {string} The hex decimal
+ */
+export function percentToHex(percent: number): string {
+  if (percent < 0 || percent > 100)
+    throw new Error('Value must in range [0, 100]');
+
+  const intValue = Math.round((percent / 100) * 255); // map percent to nearest integer (0 - 255)
+  const hexValue = intValue.toString(16); // get hexadecimal representation
+  return hexValue.padStart(2, '0').toUpperCase(); // format with leading 0 and upper case characters
+}

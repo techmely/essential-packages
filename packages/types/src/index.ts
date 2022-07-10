b2b8bae19ea4c72d30b445eae35c80d06360ebf1
Harry Tran <nhattq.coding@gmail.com>
2022-07-10 17:06:14 +0700

    feat(type): add more generate typing

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,20 +1,3 @@
 export * from './combination';
-
-export {
-  type BasePackageJson,
-  type DependencyManifest,
-  type PackageExtension
-} from '@techmely/build-configs';
-
-export type Nullable<T> = T | null;
-export type UnDef<T> = T | undefined;
-export type NullList<T> = T | undefined | null;
-
-export interface DictionaryNum<T> {
-  [id: number]: UnDef<T>;
-}
-export interface Dictionary<T> extends DictionaryNum<T> {
-  [id: string]: UnDef<T>;
-}
-
-export type VoidFunc<T> = (value: T) => void;
+export * from './literalType';
+export * from './common';
---
+++ packages/types/src/literalType.ts
@@ -0,0 +1,17 @@
+export type ComputeRange<
+  N extends number,
+  Result extends unknown[] = []
+> = Result['length'] extends N
+  ? Result
+  : ComputeRange<N, [...Result, Result['length']]>;
+
+type Enumerate<
+  N extends number,
+  Acc extends number[] = []
+> = Acc['length'] extends N
+  ? Acc[number]
+  : Enumerate<N, [...Acc, Acc['length']]>;
+
+export type INT<From extends number, To extends number> =
+  | Exclude<Enumerate<To>, Enumerate<From>>
+  | To;

244d07c274e8328713db5735d7264b485e3df228
Harry Tran <nhattq.coding@gmail.com>
2022-07-10 21:26:47 +0700

    fix(utils): seperate node vs brower utils

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,4 +1,5 @@
 export * from './vendor/dayjs';
+
 export * from './array';
 export * from './colors';
 
@@ -6,9 +7,7 @@ export * from './math/calculate';
 
 export * from './files/bytes';
 export * from './files/mimes';
-export * from './files/fs';
 export * from './files/download';
-export * from './files/findNearestFile';
 
 export * from './object';
 export * from './progressBar';
@@ -18,7 +17,6 @@ export * from './transform/convert';
 
 export * from './vector';
 export * from './generate';
-export * from './git';
 
 export * from './is/isBrowser';
 export * from './is/isCallable';
@@ -37,3 +35,8 @@ export * from './services/api.services';
 export * from './services/cookie.services';
 
 export * from './compat';
+
+// Node.js only
+// export * from './git';
+// export * from './files/fs';
+// export * from './files/findNearestFile';

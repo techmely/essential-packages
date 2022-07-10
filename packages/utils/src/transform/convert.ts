1b9918f815b14258f544edc22784ab8a7c33a8cf
Harry Tran <nhattq.coding@gmail.com>
2022-07-10 17:09:34 +0700

    feat(utils): add function alpha hex

--- packages/utils/src/transform/convert.ts
+++ packages/utils/src/transform/convert.ts
@@ -1,3 +1,5 @@
+import type { ComputeRange } from '@techmely/types';
+
 /**
  * Transform an integer into its hexadecimal value
  *
@@ -29,11 +31,32 @@ export function intToBuffer(integer: number | bigint): Buffer {
  * @param {number} percent the input value
  * @returns {string} The hex decimal
  */
-export function percentToHex(percent: number): string {
-  if (percent < 0 || percent > 100)
+export function percentToHex(
+  percent: ComputeRange<MAXIMUM_ALLOWED_BOUNDARY>[number]
+): string {
+  if (percent < 0 || percent > 100) {
     throw new Error('Value must in range [0, 100]');
+  }
 
   const intValue = Math.round((percent / 100) * 255); // map percent to nearest integer (0 - 255)
   const hexValue = intValue.toString(16); // get hexadecimal representation
   return hexValue.padStart(2, '0').toUpperCase(); // format with leading 0 and upper case characters
 }
+
+const HEX_LENGTH = 6;
+const HEX_OPACITY_LENGTH = 8;
+
+export function alphaHex(
+  hex: string,
+  alpha: ComputeRange<MAXIMUM_ALLOWED_BOUNDARY>[number]
+) {
+  if (!hex) {
+    throw new Error('Hex value is required');
+  }
+  if (hex.length === HEX_OPACITY_LENGTH) {
+    return `${hex.slice(0, HEX_LENGTH)}${percentToHex(alpha)}`;
+  }
+  return `${hex}${percentToHex(alpha)}`;
+}
+
+type MAXIMUM_ALLOWED_BOUNDARY = 101;

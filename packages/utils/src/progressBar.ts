be8c18c71033bfca3e5f32201bb6d6f15ed5d64a
Harry Tran <nhattq.coding@gmail.com>
2022-07-10 21:37:31 +0700

    fix(utils): seperate node vs brower utils

--- packages/utils/src/progressBar.ts
+++ packages/utils/src/progressBar.ts
@@ -19,66 +19,61 @@ interface Options {
 // the eighth Mersenne Prime
 const MAX_Z_INDEX = Math.pow(2, 32) - 1;
 
-export class ProgressBar {
-  /** Show the progress bar and begin animating it. */
-  start: () => void;
+export const ProgressBar = (options?: Partial<Options>) => {
+  const assign = Object.assign;
+  const config: Options = assign(
+    {
+      thickness: 2,
+      color: '#6366f1', // Indigo 500
+      className: 'progress-bar',
+      delay: 80
+    },
+    options
+  );
 
-  /** End the progress bar animation. */
-  finish: () => void;
+  const initialStyle = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    margin: 0,
+    padding: 0,
+    border: 'none',
+    borderRadius: 0,
+    backgroundColor: config.color,
+    zIndex: MAX_Z_INDEX,
+    height:
+      typeof config.thickness === 'number'
+        ? config.thickness + 'px'
+        : config.thickness,
+    color: config.color,
+    opacity: 0,
+    width: '0%'
+  };
 
-  constructor(options?: Partial<Options>) {
-    const assign = Object.assign;
-    const config: Options = assign(
-      {
-        thickness: 2,
-        color: '#6366f1', // Indigo 500
-        className: 'progress-bar',
-        delay: 80
-      },
-      options
-    );
+  const startedStyle = {
+    opacity: 1,
+    width: '99%',
+    transition: 'width 10s cubic-bezier(0.1, 0.05, 0, 1)'
+  };
 
-    const initialStyle = {
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      margin: 0,
-      padding: 0,
-      border: 'none',
-      borderRadius: 0,
-      backgroundColor: config.color,
-      zIndex: MAX_Z_INDEX,
-      height:
-        typeof config.thickness === 'number'
-          ? config.thickness + 'px'
-          : config.thickness,
-      color: config.color,
-      opacity: 0,
-      width: '0%'
-    };
+  const finishedStyle = {
+    opacity: 0,
+    width: '100%',
+    transition: 'width 0.1s ease-out, opacity 0.5s ease 0.2s'
+  };
 
-    const startedStyle = {
-      opacity: 1,
-      width: '99%',
-      transition: 'width 10s cubic-bezier(0.1, 0.05, 0, 1)'
-    };
+  const glowStyle = {
+    opacity: 0.4,
+    boxShadow: '3px 0 8px',
+    height: '100%'
+  };
 
-    const finishedStyle = {
-      opacity: 0,
-      width: '100%',
-      transition: 'width 0.1s ease-out, opacity 0.5s ease 0.2s'
-    };
+  let timeout: NodeJS.Timeout | null;
+  let current!: HTMLElement;
 
-    const glowStyle = {
-      opacity: 0.4,
-      boxShadow: '3px 0 8px',
-      height: '100%'
-    };
-
-    let timeout: NodeJS.Timeout | null;
-    let current!: HTMLElement;
-
-    this.start = () => {
+  return {
+    /** Show the progress bar and begin animating it. */
+    start() {
       if (current && current.parentNode) {
         current.parentNode.removeChild(current);
       }
@@ -101,9 +96,9 @@ export class ProgressBar {
 
       // Force a reflow, just to be sure that the initial style takes effect.
       current.scrollTop = 0;
-    };
-
-    this.finish = () => {
+    },
+    /** End the progress bar animation. */
+    finish() {
       if (timeout != null) {
         clearTimeout(timeout);
         timeout = null;
@@ -112,6 +107,6 @@ export class ProgressBar {
         current.className = config.className + ' finished';
         assign(current.style, finishedStyle);
       }
-    };
-  }
-}
+    }
+  };
+};

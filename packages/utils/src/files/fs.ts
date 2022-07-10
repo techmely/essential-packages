1550115eb0fb6c48a5c47cd0cf7d5d1b59fe1ab6
Harry Tran <nhattq.coding@gmail.com>
2022-07-10 17:08:28 +0700

    feat(utils): add fucntion find nearest files with fs

--- packages/utils/src/files/fs.ts
+++ packages/utils/src/files/fs.ts
@@ -65,3 +65,19 @@ export function copyDir(srcDir: string, destDir: string): void {
     }
   }
 }
+
+export function readFile(filePath: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (error, result) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+
+export function readFileSync(filePath: string): string {
+  return fs.readFileSync(filePath, 'utf8');
+}

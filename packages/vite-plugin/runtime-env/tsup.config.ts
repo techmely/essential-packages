aaf893ba0c6ae7340e494139a8220d00f05b373e
Harry Tran <nhattq.coding@gmail.com>
2022-08-07 20:52:52 +0700

    chore(deps): bump latest dependencies

--- packages/vite-plugin/runtime-env/tsup.config.ts
+++ packages/vite-plugin/runtime-env/tsup.config.ts
@@ -1,5 +1,22 @@
-import { getTsupOptions } from '@techmely/build-configs';
-import { defineConfig } from 'tsup';
+// import { getTsupOptions } from '@techmely/build-configs';
+// import { defineConfig } from 'tsup';
+// import pkg from './package.json';
+
+// export default defineConfig(getTsupOptions(pkg));
+
+import { defineConfig, Options } from 'tsup';
 import pkg from './package.json';
 
-export default defineConfig(getTsupOptions(pkg));
+const external = [...new Set(Object.keys(pkg?.peerDependencies ?? {}))];
+
+const universalOptions: Options = {
+  target: 'node16',
+  entry: ['src/*.ts', 'src/**/*.ts'],
+  entryPoints: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  outDir: 'build',
+  external,
+  clean: true
+};
+
+export default defineConfig([universalOptions]);

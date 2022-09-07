c34e38c297afe326d11ad4ea1058c0fab6e2644c
Harry Tran <nhattq.coding@gmail.com>
2022-09-07 07:42:34 +0700

    chore: remove unnecessary packages

--- packages/build-configs/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './tsup';
-export * from './types';
--- packages/build-configs/tsup.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'tsup';
-import { getTsupOptions } from './src';
-import pkg from './package.json';
-
-const options = getTsupOptions(pkg);
-
-export default defineConfig(options);

45704d604aec38702af418a91d5c2c440db1517f
nhat.bizz <nhattq.coding@gmail.com>
2022-10-23 09:46:44 +0700

    revise the fucntions check is somthing type in javascript

--- packages/utils/src/is/index.ts
+++ packages/utils/src/is/index.ts
@@ -17,10 +17,13 @@ export function isPrimitive(value: unknown): boolean {
 export const isDef = <T = any>(val?: T): val is T => typeof val !== "undefined";
 export const isUndef = <T = any>(val?: T): val is T => !isDef(val);
 export const isBoolean = (val: any): val is boolean => typeof val === "boolean";
-export const isFunction = <T extends Function>(val: any): val is T => typeof val === "function";
-export const isNumber = (val: any): val is number => typeof val === "number";
-export const isString = (val: unknown): val is string => typeof val === "string";
-export const isObject = (val: any): val is object => toString.call(val) === "[object Object]";
+export const isFunction = (val: any): boolean => typeof val === "function";
+export const isNumber = (val: any): val is number =>
+	toString.call(val) === "[object Number]";
+export const isString = (val: unknown): val is string =>
+	typeof val === "string";
+export const isObject = (val: any): val is object =>
+	toString.call(val) === "[object Object]";
 
 export const isFalsy = (val: any): val is false | undefined | null =>
 	isNotNull(val) && isDef(val) && isNotEmpty(val);
@@ -33,9 +36,14 @@ export const isBrowser = typeof window !== "undefined";
 
 type EmptyArray<T> = readonly [T, ...ReadonlyArray<T>];
 
-const isEmptyArr = <T>(array: ReadonlyArray<T> | undefined): array is EmptyArray<T> =>
-	array?.length === 0;
+const isEmptyArr = <T>(
+	array: ReadonlyArray<T> | undefined,
+): array is EmptyArray<T> => array?.length === 0;
 
+/**
+ * Check a value is empty or not
+ * Currently not support check WeakMap/WeakSet/WeakRef
+ */
 export function isEmpty<T = unknown>(val: T): val is T {
 	if (!val) {
 		return true;

6e188b9367276b2fd40324edc72f0e23df9f8d53
Harry Tran <nhattq.coding@gmail.com>
2023-03-23 22:27:07 +0700

    chore: upgrade yarn version + update mitosis config

--- packages/ui/mitosis.config.js
+++ packages/ui/mitosis.config.js
@@ -1,3 +1,4 @@
+/** @type {import('@builder.io/mitosis').MitosisConfig} */
 module.exports = {
 	files: "src/**",
 	targets: ["vue3", "solid", "svelte", "react"],
--- packages/ui/output/solid/src/SvgUse/SvgUse.jsx
+++ packages/ui/output/solid/src/SvgUse/SvgUse.jsx
@@ -1,9 +1,9 @@
 function SvgUse(props) {
-  return (
-    <svg class={props.className} role="img" aria-label={label} {...rest}>
-      <use href={`/images/svg/all.svg#${id}`}></use>
-    </svg>
-  );
+	return (
+		<svg class={props.className} role="img" aria-label={label} {...rest}>
+			<use href={`/images/svg/all.svg#${id}`} />
+		</svg>
+	);
 }
 
 export default SvgUse;

9c49c8584ed9b3716a6fa4a84c54300d4fc35731
Harry Tran <nhattq.coding@gmail.com>
2023-08-06 23:21:23 +0700

    bump(deps): upgrade deps

--- unocss.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig, transformerVariantGroup } from "unocss";
-import { tmlUnoConfigs } from "./packages/ui/uno.config";
-
-export default defineConfig({
-	...tmlUnoConfigs,
-	transformers: [transformerVariantGroup()],
-});

db2fa4d1a772407e816f998d93d8ee7590100dfc
Harry Tran <nhattq.coding@gmail.com>
2023-08-15 12:12:28 +0700

    feat(utils): add env shimd for all envs

---
+++ packages/utils/src/env.ts
@@ -0,0 +1,43 @@
+import { env } from "process";
+
+const _envShim = Object.create(null);
+
+export type EnvObject = Record<string, string | undefined>;
+
+const _getEnv = (useShim?: boolean) =>
+	globalThis.process?.env || // Node.js/Bun
+	(import.meta as unknown as { env: EnvObject }).env || // Vite env
+	((globalThis as any).Deno?.env.toObject() as EnvObject) || // Deno env
+	(globalThis as unknown as { __env__: EnvObject }).__env__ || // Custom env
+	(useShim ? _envShim : globalThis);
+
+export const envs = new Proxy<EnvObject>(_envShim, {
+	get(_, prop) {
+		const env = _getEnv();
+		return env[prop as any] ?? _envShim[prop];
+	},
+	has(_, prop) {
+		const env = _getEnv();
+		return prop in env || prop in _envShim;
+	},
+	set(_, prop, value) {
+		const env = _getEnv(true);
+		env[prop as any] = value;
+		return true;
+	},
+	deleteProperty(_, prop) {
+		if (!prop) {
+			return false;
+		}
+		const env = _getEnv(true);
+		delete env[prop as any];
+		return true;
+	},
+	ownKeys() {
+		const env = _getEnv();
+		return Object.keys(env);
+	},
+});
+
+export const nodeENV =
+	(typeof process !== "undefined" && process.env && process.env.NODE_ENV) || "";

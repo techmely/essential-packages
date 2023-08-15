4fc9b8526b3fcc24db9a4ea7afac3052b6d0c598
Harry Tran <nhattq.coding@gmail.com>
2023-08-15 12:12:42 +0700

    feat(utils): add process shims

---
+++ packages/utils/src/process.ts
@@ -0,0 +1,29 @@
+import { EnvObject, envs } from "./env";
+
+export interface Process extends Partial<Omit<typeof globalThis.process, "versions">> {
+	env: EnvObject;
+	versions: Record<string, string>;
+}
+
+export const _process = (globalThis.process || Object.create(null)) as unknown as Process;
+
+const processShims: Partial<Process> = {
+	versions: {},
+};
+
+export const process = new Proxy<Process>(_process, {
+	get(target, prop: keyof Process) {
+		if (prop === "env") {
+			return envs;
+		}
+		if (prop in target) {
+			return target[prop];
+		}
+		if (prop in processShims) {
+			return processShims[prop];
+		}
+	},
+});
+
+/** Value of process.platform */
+export const platform = _process.platform || "";

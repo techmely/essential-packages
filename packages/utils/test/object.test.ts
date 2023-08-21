11ebfc10b77f796436acac4973e9adf3d313444c
Harry Tran <nhattq.coding@gmail.com>
2023-08-21 16:47:06 +0700

    test(utils): add test cases for pick

--- packages/utils/test/object.test.ts
+++ packages/utils/test/object.test.ts
@@ -1,4 +1,4 @@
-import { deepMerge, objectCamel2Snake } from "../src/object";
+import { deepMerge, objectCamel2Snake, pick } from "../src/object";
 import { describe, expect, it } from "vitest";
 
 describe("Object test", () => {
@@ -27,11 +27,104 @@ describe("Object test", () => {
 		});
 	});
 	describe("Camel case object to snake case object", () => {
-		it("Should xxx", () => {
+		it("Should transform correctly", () => {
 			const obj1 = { abc: 1, helloWorld: "ok", nani1: "LOL", ohmy11God: "ohmy11God" };
 			const obj2 = { abc: 1, hello_world: "ok", nani_1: "LOL", ohmy_1_1_god: "ohmy11God" };
 			const result = objectCamel2Snake(obj1);
 			expect(result).toEqual(obj2);
 		});
+	});
+});
+
+describe("Pick", () => {
+	const data = [
+		// pick single path
+		[
+			{
+				ignored: "fake",
+				outer1: { inner1: "mock", inner2: "mock2", inner3: "mock3" },
+				outer2: "outer",
+			},
+			"outer1.inner1",
+			"mock",
+		],
+		[
+			{
+				outer1: {
+					inner1: "mock",
+					nested: {
+						nested1: {
+							nested2: "nested2",
+						},
+					},
+				},
+			},
+			"outer1.nested.nested1.nested2",
+			"nested2",
+		],
+		[
+			{
+				ignored: "fake",
+				outer1: { inner1: "mock", inner2: "mock2", inner3: "mock3" },
+				outer2: "outer",
+			},
+			"not.defined.yet",
+			undefined,
+		],
+
+		/// pick multiple paths
+		[{ name: "John", age: 30 }, [], {}],
+		[
+			{},
+			["name", "age"],
+			{
+				name: undefined,
+				age: undefined,
+			},
+		],
+		[{ name: "John", age: 30, city: "New York" }, ["name", "age"], { name: "John", age: 30 }],
+		[
+			{ name: "John", age: 30 },
+			["city", "occupation", "age"],
+			{
+				city: undefined,
+				occupation: undefined,
+				age: 30,
+			},
+		],
+		// picks nested properties
+		[
+			{
+				ignored: "fake",
+				outer1: { inner1: "mock", inner2: "mock2", inner3: "mock3" },
+				outer2: "outer",
+			},
+			["outer1.inner1", "outer1.inner2", "outer2"],
+			{
+				outer1: { inner1: "mock", inner2: "mock2" },
+				outer2: "outer",
+			},
+		],
+		[
+			{
+				outer1: { inner1: "mock", inner2: "mock2", inner3: "mock3" },
+				outer2: { nested1: { nested2: "nested2" } },
+			},
+			["outer1.inner1", "outer1.inner2", "outer2.nested1", "outer2.nested1.nested2"],
+			{
+				outer1: { inner1: "mock", inner2: "mock2" },
+				outer2: { nested1: { nested2: "nested2" } },
+			},
+		],
+		// avoids prototype pollution
+		[{ date: new Date() }, ["date.__proto__.constructor"], { date: {} }],
+		// creates paths for undefined values
+		[{ ignore: "mock" }, ["not.defined.yet"], { not: { defined: { yet: undefined } } }],
+	];
+
+	// @ts-expect-error Ignore type checks
+	it.concurrent.each(data)("picks object with %s", (object, paths, expected) => {
+		// @ts-expect-error Ignore type checks
+		expect(pick(object, paths)).toStrictEqual(expected);
 	});
 });

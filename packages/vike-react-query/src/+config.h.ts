074e191f6e5bbad82ea261d89f11e2d0d4171ed5
Harry Tran <nhattq.coding@gmail.com>
2024-01-03 12:10:37 +0700

    fix(react-query): fix warning in vike

--- packages/vike-react-query/src/+config.h.ts
+++ packages/vike-react-query/src/+config.h.ts
@@ -1,3 +1,5 @@
+import type { QueryClientConfig } from "@tanstack/react-query";
+import type { ReactNode } from "react";
 import type { Config } from "vike/types";
 
 const config: Config = {
@@ -21,6 +23,13 @@ const config: Config = {
   },
 };
 
-export default config;
+declare global {
+  namespace VikePackages {
+    interface ConfigVikeReact {
+      queryClientConfig?: QueryClientConfig;
+      FallbackErrorBoundary: ((props: { children: ReactNode }) => ReactNode) | string;
+    }
+  }
+}
 
-export * from "./react-query.d";
+export default config;

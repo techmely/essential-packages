4e5aee974b6af0dce959abcd0b111c00612fad0d
Harry Tran <nhattq.coding@gmail.com>
2024-01-14 20:26:06 +0700

    bump(types): release 1.6.1

--- packages/types/src/common.ts
+++ packages/types/src/common.ts
@@ -37,9 +37,9 @@ export type MergeInsertions<T> = T extends Record<string, any>
     }
   : T;
 
-export type DeepMerge<F, S> = MergeInsertions<{
+export type MergeDeep<F, S> = MergeInsertions<{
   [K in keyof F | keyof S]: K extends keyof S & keyof F
-    ? DeepMerge<F[K], S[K]>
+    ? MergeDeep<F[K], S[K]>
     : K extends keyof S
       ? S[K]
       : K extends keyof F
@@ -48,3 +48,4 @@ export type DeepMerge<F, S> = MergeInsertions<{
 }>;
 
 export type RuntimeEnv = "development" | "staging" | "production";
+export type NodeEnv = "development" | "test" | "production";

d9d1c633ec7c32c335e098366fc556cd9a56748b
Harry Tran <nhattq.coding@gmail.com>
2024-01-22 09:57:05 +0700

    feat(utils): add ranking array

--- packages/utils/src/ranking/index.ts
+++ packages/utils/src/ranking/index.ts
@@ -3,7 +3,7 @@ import { ranking } from "./ranking.test.mjs";
 describe("Check ranking func", () => {
   expect(ranking([8, 6, 9, 5], (a, b) => a < b)).toStrictEqual([2, 3, 1, 4]);
 
-  expect(
-    ranking([3, 1, 2, 4], (a, b) => a.toString().localeCompare(b.toString()) > 0),
-  ).toStrictEqual([3, 1, 2, 4]);
+  expect(ranking(["c", "a", "b", "d"], (a, b) => a.localeCompare(b) > 0)).toStrictEqual([
+    3, 1, 2, 4,
+  ]);
 });

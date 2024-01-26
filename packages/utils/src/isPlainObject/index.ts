bbbf8815247c0a03fb7508c071fb15cc9c1e8d91
Harry Tran <nhattq.coding@gmail.com>
2024-01-26 07:13:21 +0700

    feat: add func check value is plain object or not

---
+++ packages/utils/src/isPlainObject/index.ts
@@ -0,0 +1,25 @@
+// Reference: https://github.com/lodash/lodash/blob/master/isPlainObject.js
+export function isPlainObject(value: any) {
+  if (!isObjectLike(value) || getTag(value) !== "[object Object]") {
+    return false;
+  }
+  if (Object.getPrototypeOf(value) === null) {
+    return true;
+  }
+  let proto = value;
+  while (Object.getPrototypeOf(proto) !== null) {
+    proto = Object.getPrototypeOf(proto);
+  }
+  return Object.getPrototypeOf(value) === proto;
+}
+
+function isObjectLike(value: any) {
+  return typeof value === "object" && value !== null;
+}
+
+export function getTag(value: any) {
+  if (value == null) {
+    return value === undefined ? "[object Undefined]" : "[object Null]";
+  }
+  return Object.prototype.toString.call(value);
+}

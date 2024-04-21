8b3540333131ce791db5db439c3bdaa586fd83b0
Harry Tran <nhattq.coding@gmail.com>
2024-04-21 22:38:21 +0700

    workflow: update tsconfig + moon config

--- ts-packages/utils/src/delaySignal/index.ts
+++ ts-packages/utils/src/delaySignal/index.ts
@@ -11,7 +11,7 @@ export async function delaySignal(ms: number, { signal }: DelayOptions): Promise
 
     function abortHandler() {
       clearTimeout(timeoutId);
-      reject(signal!.reason);
+      reject(signal?.reason);
     }
 
     const timeoutId = setTimeout(() => {

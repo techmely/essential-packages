30b6c55e80da8b085247e0be0901465010490237
harrytran998 <nhattq.coding@gmail.com>
2024-04-25 12:17:36 +0700

    feat(ddd): add domain events

---
+++ ts-packages/domain-driven/src/domain/events/index.ts
@@ -0,0 +1,87 @@
+import { validateContextEventName } from "../context/context.utils";
+import type { ContextEventName } from "../context/types";
+import type {
+  DomainEvent,
+  DomainEventHandler,
+  DomainEventMetrics,
+  DomainEventOptions,
+  DomainEventPort,
+  DomainEventPromiseHandler,
+} from "./types";
+
+export class DomainEvents<T> implements DomainEventPort<T> {
+  #metrics: DomainEventMetrics;
+  #events: DomainEvent<T>[];
+  #totalDispatched: number;
+
+  constructor(private readonly aggregate: T) {
+    this.#events = [];
+    this.#totalDispatched = 0;
+    this.#metrics = {
+      totalDispatched: () => this.#totalDispatched,
+      totalEvents: () => this.#events.length,
+    };
+  }
+
+  get metrics(): DomainEventMetrics {
+    return this.#metrics;
+  }
+
+  dispatch(name: ContextEventName, ...args: unknown[]): void | Promise<void> {
+    const event = this.#events.find((e) => e.name === name);
+    if (!event) return;
+    this.#totalDispatched += 1;
+    event.handler(this.aggregate, [event, ...args]);
+    this.remove(name);
+  }
+
+  add(
+    name: ContextEventName,
+    handler: DomainEventHandler<T>,
+    _options?: DomainEventOptions | undefined,
+  ): void {
+    validateContextEventName(name);
+    if (typeof handler !== "function")
+      throw new Error(`Add event handler for ${name} must be a function`);
+    const options: DomainEventOptions = _options ? _options : { priority: this.#getPriority() };
+    this.remove(name);
+    this.#events.push({ name, handler, options });
+  }
+
+  clear(): void {
+    this.#events = [];
+  }
+
+  remove(name: ContextEventName): void {
+    this.#events = this.#events.filter((e) => e.name !== name);
+  }
+
+  async dispatchEvents(): Promise<void> {
+    const pEvents: DomainEventPromiseHandler<T>[] = [];
+    const sortedEvents = this.#events.sort((a, b) => a.options.priority - b.options.priority);
+    for (const event of sortedEvents) {
+      this.#totalDispatched += 1;
+      const fn = event.handler(this.aggregate, [event]);
+      if (fn instanceof Promise) {
+        pEvents.push(fn as unknown as DomainEventPromiseHandler<T>);
+      }
+    }
+    /**
+     * This is a happy case we assume that all promises run well
+     * We might improve this workflow bcs might be some case not success will effect to all the promises
+     */
+    try {
+      await Promise.all(pEvents);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      this.clear();
+    }
+  }
+
+  #getPriority() {
+    const totalEvents = this.#events.length;
+    if (totalEvents <= 1) return 2;
+    return totalEvents;
+  }
+}
---
+++ ts-packages/domain-driven/src/domain/events/types.ts
@@ -0,0 +1,42 @@
+import type { ContextEventName } from "../context/types";
+
+export type DomainEventMetrics = {
+  totalEvents: () => number;
+  totalDispatched: () => number;
+};
+
+type HandlerArgs<T> = [T, [DomainEvent<T>, ...any[]]];
+
+export type DomainEventPromiseHandler<T> = (...args: HandlerArgs<T>) => Promise<void>;
+
+type NormalHandler<T> = (...args: HandlerArgs<T>) => void;
+
+export type DomainEventHandler<T> = DomainEventPromiseHandler<T> | NormalHandler<T>;
+
+export type DomainEventOptions = {
+  priority: number;
+};
+
+export type DomainEvent<T> = {
+  name: string;
+  handler: DomainEventHandler<T>;
+  options: DomainEventOptions;
+};
+
+export interface DomainEventPort<T> {
+  dispatch(name: ContextEventName, ...args: unknown[]): void | Promise<void>;
+  add(name: ContextEventName, handler: DomainEventHandler<T>, options?: DomainEventOptions): void;
+  clear(): void;
+  remove(name: ContextEventName): void;
+  dispatchEvents(): Promise<void>;
+}
+
+export abstract class AggregateEventHandler<T> {
+  constructor(readonly params: DomainEvent<T>) {
+    if (typeof params.name !== "string") throw new Error("Event name must be a string");
+    this.dispatch = this.dispatch.bind(this);
+  }
+
+  abstract dispatch(aggregate: T, args: [DomainEvent<T>, any[]]): void;
+  abstract dispatch(...args: HandlerArgs<T>): void;
+}
